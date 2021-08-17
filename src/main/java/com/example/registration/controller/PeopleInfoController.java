b68dbe75053d2e230511508760f220ed30590d46
KARTHICKMP <karthicksiva36@yahoo.com>
2021-08-17 16:12:03 +0200

    removed unused library in controller java file

--- src/main/java/com/example/registration/controller/PeopleInfoController.java
+++ src/main/java/com/example/registration/controller/PeopleInfoController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.registration.model.PeopleInfo;
 import com.example.registration.service.PeopleInfoService;
+
 
 @Controller
 public class PeopleInfoController {
